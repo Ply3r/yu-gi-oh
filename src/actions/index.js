ced60f159799beff383d2399683b5526423e9b0e
Leandro <play3rtox1c@gmail.com>
2021-10-14 18:09:38 -0300

    teste

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ import { GAME_CHANGE } from '../store';
 
 const gameAction = (array) => {
   const metade = 0.5;
-  if (!array) return;
   const sorted = array.sort(() => Math.random() - metade);
   return ({
     type: GAME_CHANGE,
